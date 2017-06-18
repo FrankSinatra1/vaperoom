8d070619af1b2739b6ba38c38a7f77b8554da47a
FrankSinatra1 <smarttv201404@gmail.com>
2017-06-19 02:56:40 +0300

    media

--- js/main.js
+++ js/main.js
@@ -4,20 +4,45 @@ $(document).ready(function() {
 	});
 });
 
-$(document).ready(function(){
-    let owl = $(".owl-carousel"),
-        prev = $("#left"),
-        next = $("#right");
-    owl.on("changed.owl.carousel", function(){
-        next.on("click", ()=>{
-            owl.trigger("next.owl.carousel")
-        });
-        prev.on("click", ()=>{
-            owl.trigger("prev.owl.carousel");
-        })
-    })
-})
 
+$(document).ready(function(){
+  let owl = $(".owl-carousel"),
+      url = null,
+      bgDrop = $(".bg-drop"),
+      prev = $("#left"),
+      next = $("#right");
+
+  owl.owlCarousel({
+    loop: true,
+    margin: 50,
+    center: true,
+    margin: 80,
+    item: 1,
+    responsive: {
+      0: {
+        items: 3,
+      },
+      640: {
+      },
+      1224: {
+      },
+    }
+  });
+  owl.on("changed.owl.carousel", function(){
+    setTimeout(() => {
+      let centerImg = owl.find(".center").find("div"),
+          url = centerImg.css('background-image');
+      bgDrop.css('background-image', url);
+    }, 20)
+  });
+
+  next.on("click", ()=>{
+    owl.trigger("next.owl.carousel");
+  });
+  prev.on("click", ()=>{
+    owl.trigger("prev.owl.carousel");
+  })
+})
 
 
 
