bf49f1cbd823689afee0469cf61008d552ed26da
FrankSinatra1 <smarttv201404@gmail.com>
2017-06-19 00:58:27 +0300

    recode

--- js/main.js
+++ js/main.js
@@ -3,3 +3,23 @@ $(document).ready(function() {
         navigation: true,
 	});
 });
+
+$(document).ready(function(){
+    let owl = $(".owl-carousel"),
+        prev = $("#left"),
+        next = $("#right");
+    owl.on("changed.owl.carousel", function(){
+        next.on("click", ()=>{
+            owl.trigger("next.owl.carousel")
+        });
+        prev.on("click", ()=>{
+            owl.trigger("prev.owl.carousel");
+        })
+    })
+})
+
+
+
+
+
+
